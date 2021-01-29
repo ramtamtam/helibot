dce013f565609e693ff2e7c5f196a59d7ace2d26
ramtamtam <ramtamtam84@gmail.com>
2021-01-29 20:19:34 +0100

    Added welcome msg on connect

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.channels.cache
+    .get(`800820544953647194`)
+    .send(`I'm ready to play! Try me 😆`);
 });
 
 client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.channel == '800820544953647194') {
+    if (msg.content === 'ping') {
+      // msg.reply('Pong!');
+      msg.channel.send('Pong!');
+    }
+  } else {
+    if (msg.content === 'ping') {
+      msg.author.send('FU!, use bottest channel!');
+      console.log(msg.author);
+      msg.delete();
+    }
   }
 });
 
